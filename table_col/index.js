90c077620dbcab92a32d3ea69a19187d8b74c3f2
bnckncsk <koncsik.bence_2022b@bolyai-szakkozep.hu>
2025-11-01 12:39:59 +0100

    10.18 Hazi (table_col JS form letrehozas)

--- table_col/index.js
+++ table_col/index.js
@@ -78,3 +78,53 @@ function createCellElement(cellType, cellContent, cellRow) {
     cellRow.appendChild(a);
     return a;
 }
+
+// ---------------------------------
+// hf 10.18
+
+const h2 = document.createElement('h2'); // h2 címsor létrehozása + beillesztése
+h2.innerText = 'JavaScript form';
+document.body.appendChild(h2);
+
+const form = document.createElement('form'); // form létrehozása + beillesztése
+document.body.appendChild(form);
+form.id = 'form_js'; // form id megadása
+
+
+createFormElement('text', 'Költő neve:', 'kolto_nev'); // form feltöltése
+createFormElement('text', 'Korszak:', 'korszak');
+createFormElement('text', 'Szerelme:', 'szerelem1');
+createFormElement('text', 'Szerelme:', 'szerelem2');
+
+const submitButton = document.createElement('button'); // Hozzáadás gomb létrehozása
+submitButton.innerText = 'Hozzáadás';
+form.appendChild(submitButton);
+
+
+/**
+ * createFormElement() - label és input mezőt létrehoz, hozzáfűzi a formhoz
+ * @param {string} inputType - input típusa
+ * @param {string} labelContent - a labelbe írt szöveg
+ * @param {string} inputId - input id-ja
+ * @returns {HTMLFormElement} - form elementtel tér vissza
+ */
+
+function createFormElement(inputType, labelContent, inputId) {
+    const label = document.createElement('label'); // létrehozzuk a labelt
+    label.innerText = labelContent; // labelContent = a label szövege
+    label.htmlFor = inputId; // label for megegyezik az input Id-jával
+    form.appendChild(label); // hozzáadjuk a fomrhoz
+
+    const br = document.createElement('br'); // szükséges sortörések a kinézethez
+    const br2 = document.createElement('br');
+    const br3 = document.createElement('br');
+    form.appendChild(br);
+    
+    const input = document.createElement('input'); // létrehozzuk az input mezőt
+    input.id = inputId; // inputId megadása
+    input.name = inputId; // input name megegyezik az inputId-val (ebben az esetben)
+    input.type = inputType; // input típus megadása (text)
+    form.appendChild(input); // hozzáadjuk a formhoz
+    form.appendChild(br2); // sortörés
+    form.appendChild(br3); // sortörés
+}
